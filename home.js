3d9e3472128f20e991aee487f385fa4679b76673
Dani János <dani.janos@gmail.com>
2018-05-09 09:29:24 +0200

    Add name property to home custom element

--- home.js
+++ home.js
@@ -1,6 +1,12 @@
-xtag.create('x-home', class extends XTagElement {
+const Home = xtag.create('x-home', class extends XTagElement {
+
+  name (){ return 'Home page'; }
+
   '::template(true)' (){
-      return `<h2>I am the home page</h2>
+      return `<h2>I am the ${this.name()}</h2>
             <span>I was created by x-tag</span>`
   }
 });
+
+const HomeNode = new Home();
+HomeNode.render();
