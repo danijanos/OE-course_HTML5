e331d047ecab63e3a5c2225456cab1078a7a08cd
Dani János <dani.janos@gmail.com>
2018-05-09 09:51:25 +0200

    Add new template to the home x-tag
    Heroselector template added, but its not working! Bugfix needed!

--- home.js
+++ home.js
@@ -1,12 +1,16 @@
 const Home = xtag.create('x-home', class extends XTagElement {
 
-  name (){ return 'Home page'; }
+  name() {
+    return 'Home page';
+  }
 
-  '::template(true)' (){
-      return `<h2>I am the ${this.name()}</h2>
+  'main::template(true)' () {
+    return `<h2>I am the ${this.name()}</h2>
             <span>I was created by x-tag</span>`
   }
-});
 
-const HomeNode = new Home();
-HomeNode.render();
+  'heroselector::template' () {
+    return `heroselector`
+  }
+
+});
